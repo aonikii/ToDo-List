b81fb111100125fea2f303f78d525bfba3d1275b
Andrey <andreyonikii@gmail.com>
2025-05-19 00:37:07 +0300

    Разное поведение welcome - страницы в зависимости от аутентификации пользователя

--- handlers/app.go
+++ handlers/app.go
@@ -5,6 +5,14 @@ import (
 )
 
 func WelcomeHandler(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "session")
+	_, ok := session.Values["user_id"]
+
+	if ok {
+		templates.ExecuteTemplate(w, "enter.html", map[string]string{"IsAuthenticated": "Вы уже вошли в систему, можете перейти к задачам"})
+		return
+	}
+
 	if r.Method == "GET" {
 		templates.ExecuteTemplate(w, "enter.html", nil)
 	}
